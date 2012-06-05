8d41a45fbd9baae20df3ab1c758c87c8c8b53072
Masahiro Ide <imasahiro@users.sourceforge.jp>
2012-06-05 18:59:19 +0900

    add tests

---
+++ tests/function.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+struct x;
+typedef struct x X;
+void f(int a, int b, int c);
+struct x *g(int a, int b, int c);
+X *g(int a, int b, int c);
---
+++ tests/struct.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+struct S {
+    int a;
+    short b;
+    long c;
+    float d;
+    double e;
+    int (*f)(int x, short y);
+};
