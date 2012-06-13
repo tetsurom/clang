faed69c8d4e919ffa7679ee5288d5bf23effe039
uchida <uchida.atsushi.8@gmail.com>
2012-06-13 17:58:18 +0900

    add test for functions

---
+++ tests/func_array.c
@@ -0,0 +1,4 @@
+int func1(char *a);
+int func2(char b[256]);
+int func3(char c[256][256]);
+int func4(char d[256][256][256]);
---
+++ tests/func_pointer.c
@@ -0,0 +1,3 @@
+void quick_sort (void *const pbase, size_t total_elems, size_t size, int (*cmp)(const void*,const void*));
+typedef void (*returned_func)(void);
+returned_func func(int ***a);
